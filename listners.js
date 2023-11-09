317d4d3d8816c0ed09073dc1f9d02bbe6d6b0f5e
Xavier Rivera <xrivera@Xaviers-MacBook-Air.local>
2023-11-08 22:50:55 -0500

    added style to UI

---
+++ listners.js
@@ -0,0 +1,9 @@
+$(".warbler-img").on("click", () => {
+  window.location.assign("https://riverax98-projects-warbler.onrender.com");
+});
+$(".lunchly-img").on("click", () => {
+  window.location.assign("https://riverax98-projects-lunchly.onrender.com");
+});
+$(".cupcakes-img").on("click", () => {
+  window.location.assign("https://riverax98-projects-cupcakes.onrender.com");
+});
